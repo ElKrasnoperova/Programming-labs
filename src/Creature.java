7de24190a13df5fae8697d80fd42c7fc70f946ec
Elizaveta Krasnoperova <kelizaveta130598@gmail.com>
2017-03-24 20:57:17 +0400

    Init commit

---
+++ src/Creature.java
@@ -0,0 +1,24 @@
+public abstract class Creature {
+
+		protected String name;
+		protected int age;
+		protected int health;
+		protected boolean alive;
+		
+		protected Place place; 
+		
+		public Creature(String name, int age, int health) {
+			this.name = name;
+			this.age = age;
+			this.health = health;
+			this.alive = true;
+		}
+		
+		public void changePlace(Place place) {
+			this.place = place;
+		}
+		
+		public void changeHealth(int health) {
+			this.health = health;
+		}
+	}
---
+++ src/Place.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Place {
+		protected String name;
+		protected boolean isCataclysmed;
+		protected List<Person> people = new ArrayList<Person>();
+		protected List<Monster> monsters = new ArrayList<Monster>();
+
+	public Place(String name) {
+		this.name = name;		
+		this.isCataclysmed = false;
+	}
+	
+	public void setCataclysm() {
+		this.isCataclysmed = true;
+		for (Person p: people) {
+			p.changeHealth(0);
+		}
+		for (Monster m: monsters) {
+			m.changeHealth(0);
+		}
+	}
+	
+	public void addPerson(Person person) {
+		this.people.add(person);
+		for (Monster m: monsters) {
+			if(m.isSmell()) {
+				person.changeFeelSmell(true);
+				break;
+			}
+		}
+	}
+	
+	public void addMonster(Monster monster) {
+		this.monsters.add(monster);
+	}
+}

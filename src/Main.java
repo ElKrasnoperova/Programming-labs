2224009998350d63d1ec478c4c01ac05e3b04174
Elizaveta Krasnoperova <kelizaveta130598@gmail.com>
2017-03-24 21:04:13 +0400

    Fix Main

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
 		Person person1 = new Person ("person1", 40, 100);
 		Person person2 = new Person ("person2", 14, 100);
 		Person person3 = new Person ("person3", 21, 100);
-		Monster monster0 = new Monster ("monster0", 1100, 100, 2, 
-										new Complex(1, 5), 1);
-		Monster monster1 = new Monster ("monster1", 321, 100, 8, 
-										new Complex (4, 2), 3);
+		Monster monster0 = new Monster ("monster0", 1100, 100, 2, new Complex(1, 5), 1);
+		Monster monster1 = new Monster ("monster1", 321, 100, 8, new Complex (4, 2), 3);
 		Place place0 = new Place ("place0");
 		Place place1 = new Place ("place1");
 		
